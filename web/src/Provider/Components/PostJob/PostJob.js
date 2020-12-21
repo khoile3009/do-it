91b47d16c272023a0d2bcc63b0be0e960ec8a32e
HA <hoanganh.theodore@icloud.com>
2020-12-21 16:26:45 -0500

    Created basic card layout for new job post

---
+++ web/src/Provider/Components/PostJob/PostJob.js
@@ -0,0 +1,100 @@
+import React, { Modal, useState } from "react";
+import "./PostJob.scss";
+import "../../../Theme/__global-theme.scss";
+import Calendar from "react-calendar";
+import TimePicker from "react-time-picker";
+import ChipInput from "material-ui-chip-input";
+
+import {
+	FormControl,
+	InputLabel,
+	Input,
+	makeStyles,
+	Typography,
+	TextField,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		"& > *": {
+			margin: theme.spacing(1),
+		},
+	},
+	paper: {
+		position: "absolute",
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
+}));
+
+const PostJob = (props) => {
+	const classes = useStyles();
+	const [valueDate, onChangeDate] = useState(new Date());
+	const [valueTimeStart, onChangeTimeStart] = useState("10:00");
+	const [valueTimeEnd, onChangeTimeEnd] = useState("11:00");
+
+	return (
+		<div className={classes.paper}>
+			<form className={classes.root}>
+				<Typography component="h5" variant="h5">
+					Create a new job posting
+				</Typography>
+				<div>
+					<FormControl>
+						<InputLabel htmlFor="job-title">Title</InputLabel>
+						<Input id="job-title"></Input>
+					</FormControl>
+				</div>
+				<div>
+					<TextField
+						id="job-description-field"
+						label="Job Description"
+						multiline
+						fullWidth
+					></TextField>
+				</div>
+				<div>
+					<TextField id="job-location" label="Location" fullWidth></TextField>
+				</div>
+				<div>
+					<Typography component="subtitle1" variant="subtitle1">
+						Date
+					</Typography>
+					<Calendar
+						selectRange={true}
+						onChange={onChangeDate}
+						value={valueDate}
+					></Calendar>
+				</div>
+				<div>
+					<Typography component="subtitle1" variant="subtitle1">
+						Time
+					</Typography>
+					<br></br>
+					<TimePicker
+						id="time-pick-start"
+						onChange={onChangeTimeStart}
+						value={valueTimeStart}
+					></TimePicker>{" "}
+					to{" "}
+					<TimePicker
+						id="time-pick-end"
+						onChange={onChangeTimeEnd}
+						value={valueTimeEnd}
+					></TimePicker>
+				</div>
+				<div>
+					<Typography component="subtitle1" variant="subtitle1">
+						Time
+					</Typography>
+					<br />
+					<ChipInput />
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default PostJob;
---
+++ web/src/Provider/Containers/PostJob/PostJobContainer.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import PostJob from "../../Components/PostJob/PostJob.js";
+
+class PostJobContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tags: [],
+		};
+
+		this.handleKeyDown = this.handleKeyDown.bind(this);
+	}
+
+	handleKeyDown(e) {
+		if (e.key == "Enter") {
+			this.handleAdd();
+		}
+	}
+
+	handleAdd(tag) {
+		this.setState({
+			tags: [...this.state.tags, tag],
+		});
+	}
+
+	handleDelete(deletedTag) {
+		this.setState({
+			tags: this.state.tags.filter((c) => c !== deletedTag),
+		});
+	}
+
+	render() {
+		return (
+			<>
+				<PostJob></PostJob>
+				{/* <ChipInput
+					{...this.props}
+					value={this.state.tags}
+					onAdd={(tag) => this.handleAdd(tag)}
+					onDelete={(deletedTag) => this.handleDelete(deletedTag)}
+					fullWidth
+					handleKeyDown={this.handleKeyDown}
+				></ChipInput> */}
+			</>
+		);
+	}
+}
+
+export default PostJobContainer;
