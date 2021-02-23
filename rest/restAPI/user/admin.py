26fd941257c3962d48154f7d8d48dcc3bbd20cec
Khoi Le <khoibaka@gmail.com>
2021-02-22 21:36:33 -0500

    Finish configuring auth0

--- rest/restAPI/user/admin.py
+++ rest/restAPI/user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from . import models
 
 # Register your models here.
-admin.site.register([models.User, models.Customer, models.Provider])
+admin.site.register([models.Customer, models.Provider])
+admin.site.register(models.User, UserAdmin)
