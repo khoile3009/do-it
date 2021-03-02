d0196015bfd0e004860091e398379dfccda4a1d0
HieuPham9720 <hmp43@case.edu>
2021-03-01 23:27:11 -0500

    added merge confliect stuffs

---
+++ rest/restAPI/authentication/urls.py
@@ -0,0 +1,11 @@
+from .api import RegisterAPI, LoginAPI, UserAPI
+from knox import views as knox_views
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('knox.urls')),
+    path('register', RegisterAPI.as_view()),
+    path('login', LoginAPI.as_view()),
+    path('user', UserAPI.as_view()),
+    path('logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+]
