6410922d5f2fd2108e84826bec1633f27539ab2f
HA <hoanganh.theodore@icloud.com>
2021-05-06 23:57:30 -0400

    basic styling, added required textfiels for demo

--- next-app/General/Auth/RegisterModal.js
+++ next-app/General/Auth/RegisterModal.js
@@ -12,6 +12,39 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Paper from "@material-ui/core/Paper";
 import Draggable from "react-draggable";
+import TextField from "@material-ui/core/TextField";
+import { Typography, useMediaQuery } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { colors } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		"& .MuiTextField-root": {
+			margin: theme.spacing(2),
+		},
+	},
+	registerModalWrapper: {
+		padding: "1rem",
+	},
+	form: {
+		display: "flex",
+		flexDirection: "column",
+		width: "fit-content",
+	},
+	formControl: {
+		marginTop: theme.spacing(2),
+		minWidth: 120,
+	},
+	formControlLabel: {
+		marginTop: theme.spacing(1),
+	},
+	headerColor: {
+		color: "#fff",
+	},
+	registerHeader: {
+		background: "#22d469",
+	},
+}));
 
 function RegisterModalWrapper(props) {
 	return (
@@ -35,13 +68,18 @@ export default function RegisterModal(props) {
 	const dispatch = useDispatch();
 	const auth = useSelector((state) => state.auth);
 
-	// ui
+	// theme and ui
+	const classes = useStyles();
 	const [open, setOpen] = useState(true);
+	const theme = useTheme();
+	const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+	// const fullWidth = useState(false);
+	// const maxWidth = useState("md");
 
+	// events
 	const handleClickOpen = () => {
 		setOpen(true);
 	};
-
 	const handleClose = () => {
 		// submitRegisterForm();
 		setOpen(false);
@@ -50,14 +88,28 @@ export default function RegisterModal(props) {
 	return (
 		<div>
 			<Dialog
+				className={classes.registerModalWrapper}
 				open={open}
 				onClose={handleClose}
 				RegisterModalWrapper={RegisterModalWrapper}
 				aria-labelledby="register-modal"
+				fullScreen={fullScreen}
+				// fullWidth={fullWidth}
+				// maxWidth={maxWidth}
 			>
-				<DialogTitle style={{ cursor: "move" }} id="register-modal-title">
-					Registration
+				<DialogTitle
+					className={classes.registerHeader}
+					style={{ cursor: "move" }}
+					id="register-modal-title"
+				>
+					<Typography className={classes.headerColor} variant="h4">
+						Registration
+					</Typography>
 				</DialogTitle>
+				<form className={classes.registerModalWrapper + " " + classes.root}>
+					<TextField required id="first_name" label="First Name" placeholder="Anh" />
+					<TextField required id="last_name" label="Last Name" placeholder="Nguyen" />
+				</form>
 			</Dialog>
 		</div>
 	);
