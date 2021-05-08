ed0c0d3437b208376b68a3fe1df0d45b3039375b
HA <hoanganh.theodore@icloud.com>
2021-05-08 18:20:01 -0400

    modded landing page

--- next-app/General/NavigationBar/NavigationBar.js
+++ next-app/General/NavigationBar/NavigationBar.js
@@ -142,6 +142,7 @@ const useStyles = makeStyles((theme) => ({
 	content: {
 		flexGrow: 1,
 		padding: theme.spacing(3),
+		paddingBottom: "0",
 		transition: theme.transitions.create("margin", {
 			easing: theme.transitions.easing.sharp,
 			duration: theme.transitions.duration.leavingScreen,
