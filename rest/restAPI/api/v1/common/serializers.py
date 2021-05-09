f5a45ab52b55bd0683e561076260d886a060d1e8
Khoi Le <khoibaka@gmail.com>
2021-05-08 21:01:42 -0400

    Remove parent modelserializer in category

--- rest/restAPI/api/v1/common/serializers.py
+++ rest/restAPI/api/v1/common/serializers.py
@@ -4,7 +4,6 @@ from common import models
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    parent = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = models.Category
         fields = [
