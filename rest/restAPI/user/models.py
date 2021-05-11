80dd9d445a3570569572fee377e3d2c9afef17dd
HieuPham9720 <hmp43@case.edu>
2021-05-11 05:56:25 -0400

    move manager to models like django

--- rest/restAPI/user/models.py
+++ rest/restAPI/user/models.py
@@ -7,7 +7,31 @@ from common import models as common_models
 
 from datetime import date
 
-from . import manager
+
+class UserManager(auth_models.BaseUserManager):
+
+    def create_user(self, email, password, phone_number):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not password:
+            raise ValueError("User must have an password")
+        if not phone_number:
+            raise ValueError("User must have an phone number")
+
+        normalized_email = self.normalize_email(email)
+        user = self.model(email=normalized_email, username=normalized_email, phone_number=phone_number)
+        user.set_password(password)
+        user.save()
+        
+        return user
+
+    def create_superuser(self, email, password, phone_number):
+        user = self.create_user(email, password, phone_number)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
 
 
 class User(auth_models.AbstractUser):
@@ -44,7 +68,7 @@ class User(auth_models.AbstractUser):
 
     is_provider = models.BooleanField(default=False)    
 
-    objects = manager.UserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['phone_number']
