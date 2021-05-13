d2488392699a1544afc2de037284ddd755cedf30
Khoi Le <khoibaka@gmail.com>
2021-05-13 16:20:22 -0400

    Add user skills api

--- rest/restAPI/api/v1/user/api.py
+++ rest/restAPI/api/v1/user/api.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from common.models import Category
 from user.models import UserSkill
 from django.http import HttpResponse, JsonResponse
+import json
 
 
 class UserSkillAPI(generics.GenericAPIView):
@@ -12,13 +13,33 @@ class UserSkillAPI(generics.GenericAPIView):
 
     def post(self, *args, **kwargs):
         user = self.request.user
-        if not self.request.POST.get('skill_id'):
+        data = json.loads(self.request.body)
+        if not data.get('skill_id'):
             return JsonResponse({'status': 'No skill_id provided'}, status=422)
         try:
             # TODO: Recursively add subskils
-            skill = Category.objects.get(id=self.request.POST['skill_id'])
+            skill = Category.objects.get(id=data['skill_id'])
         except Category.DoesNotExist:
             return JsonResponse({'status': 'No skill with that id found '}, status=404)
 
         user_skill = UserSkill.objects.get_or_create(user=user, skill=skill)
+        return JsonResponse({'status': 'ok'}, safe=True)
+
+    def delete(self, *args, **kwargs):
+        user = self.request.user
+        data = json.loads(self.request.body)
+        if not data.get('skill_id'):
+            return JsonResponse({'status': 'No skill_id provided'}, status=422)
+        try:
+            # TODO: Recursively add subskils
+            skill = Category.objects.get(id=data['skill_id'])
+        except Category.DoesNotExist:
+            return JsonResponse({'status': 'No skill with that id found '}, status=404)
+
+        user_skill = UserSkill.objects.get(user=user, skill=skill)
+        if not user_skill:
+            return JsonResponse({'status': 'No skill with that id found on user'}, status=404)
+        if user_skill.number_rating != 0:
+            return JsonResponse({'status': 'Cannot delete skill with rating'}, status=404)
+        user_skill.delete()
         return JsonResponse({'status': 'ok'}, safe=True)
