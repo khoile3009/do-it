2fdc1443fca950fb2ae31cad11be473dff4b449b
Khoi Le <khoibaka@gmail.com>
2021-05-15 15:53:47 -0400

    Add first_name and lastname to user create

--- rest/restAPI/api/v1/user/serializers.py
+++ rest/restAPI/api/v1/user/serializers.py
@@ -28,27 +28,27 @@ class UserSerializer(serializers.ModelSerializer):
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = User
         fields = [
-            'id', 
-            'username', 
+            'id',
+            'username',
             'email',
-            'password', 
-            'first_name', 
+            'password',
+            'first_name',
             'last_name',
             'phone_number',
         ]
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(username=validated_data['username'],
-                                        email=validated_data['email'],
-                                        password=validated_data['password'],
-                                        first_name=validated_data['first_name'],
-                                        last_name=validated_data['last_name'],
-                                        phone_number=validated_data['phone_number'])
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            phone_number=validated_data['phone_number'])
 
         return user
 
